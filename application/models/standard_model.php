2f9f0dd9f3b0b0f4a34f6eb6a9c81c75e71d7cd5
root <ravisharma972@gmail.com>
2017-07-03 10:18:42 +0530

    first commit

---
+++ application/models/standard_model.php
@@ -0,0 +1,178 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+  
+Class Standard_model extends CI_Model
+{
+    function __construct()
+    {
+      parent::__construct();
+    }
+    private $table = null;
+    private $condition = null;
+    private $join = null;
+    private $right_join = null;
+    private $left_join = null;
+    private $field = null;
+    private $select_as_array = null;
+    private $limit = null;
+    private $offset = 0;
+    private $groupby = null;
+    private $order_by = null;
+    private $where_in = null;
+    private $or_where_in = null;
+    private $having = null;
+    private $insert_batch = null;
+    private $update_batch = null;
+    private $database = null;
+    private $like = null;
+
+    /* This function used to set private variables which is required for DB queries*/
+    public function set_query_data($data){
+        if( count($data) ){
+            
+            $this->table = ( isset($data['table']) ) ? $data['table'] :  null;
+            if($this->table == null){
+                return "missing table name";
+            }
+            $this->condition       = ( isset($data['condition']) ) ? $data['condition'] :  null;
+            $this->having       = ( isset($data['having']) ) ? $data['having'] :  null;
+            $this->field           = ( isset($data['field']) ) ? $data['field'] :  null;
+            $this->select_as_array = ( isset($data['select_as_array']) ) ? $data['select_as_array'] : false;
+            $this->join            = ( isset($data['join']) ) ? $data['join'] :  null;
+            $this->right_join      = ( isset($data['right_join']) ) ? $data['right_join'] :  null;
+            $this->left_join       = ( isset($data['left_join']) ) ? $data['left_join'] :  null;
+            $this->groupby         = ( isset($data['group_by'])) ? $data['group_by'] : null;
+            $this->order_by        = ( isset($data['order_by'])) ? $data['order_by'] : null;
+            $this->limit           = ( isset($data['limit'])) ? $data['limit'] : null;
+            $this->offset          = ( isset($data['offset'])) ? $data['offset'] : null;            
+            $this->where_in        = ( isset($data['where_in'])) ? $data['where_in'] : null;
+            $this->or_where        = ( isset($data['or_where'])) ? $data['or_where'] : null;
+            $this->or_where_in     = ( isset($data['or_where_in'])) ? $data['or_where_in'] : null;
+            $this->insert_batch    = ( isset($data['insert_batch'])) ? $data['insert_batch'] : null;
+            $this->update_batch    = ( isset($data['update_batch'])) ? $data['update_batch'] : null;
+            $this->database        = ( isset($data['database'])) ? $this->load->database($data['database'], true) : $this->load->database('default', true);
+            $this->like            = ( isset($data['like'])) ? $data['like'] : null;
+            
+        }
+        else{
+            return "missing parmeters";
+        }
+    }
+    public function select(){
+        if( !$this->database->table_exists( $this->table ) )
+        {
+            return;
+        }
+            
+        
+        $this->database->select( $this->field )->from( $this->table );
+        if( $this->join ){ 
+        // Here check for join is set if it is join operation is performed
+            foreach ( $this->join as $table_name => $join_condition ) {
+                $this->database->join( $table_name , $join_condition);
+            }
+        }
+        if( $this->right_join ){ 
+        // Here check for join is set if it is join operation is performed
+            foreach ( $this->right_join as $table_name => $join_condition ) {
+                $this->database->join( $table_name , $join_condition, 'RIGHT');
+            }
+        }
+        if( $this->left_join ){ 
+        // Here check for join is set if it is join operation is performed
+            foreach ( $this->left_join as $table_name => $join_condition ) {
+                $this->database->join( $table_name , $join_condition, 'LEFT');
+            }
+        }
+        if( $this->condition ){
+            // Here check for condition if condition is set where condition is applied
+            $this->database->where( $this->condition );
+        }        
+        if($this->or_where){
+            $this->database->or_where( $this->or_where );
+        }
+        if($this->where_in){
+            if(isset($this->where_in['where_in_field'])){
+                $this->database->where_in( $this->where_in['where_in_field'], $this->where_in['where_in_array'] );
+            }else{
+
+                $this->database->where_in( $this->where_in );
+            }
+        }
+        if($this->or_where_in){
+            $this->database->where_in( $this->or_where_in['or_where_in_field'], $this->or_where_in['where_in_array'] );
+        }
+        if($this->groupby){
+            foreach ( $this->groupby as  $groupby ) {
+                $this->database->group_by( $groupby );
+            }
+            
+        }
+        if( $this->having ){
+          // Here applying condition with having clause
+          $this->database->having( $this->having );
+        }        
+        if($this->order_by){            
+            if(isset($this->order_by[0])) {
+                foreach ( $this->order_by as $order_by ) {
+                    $this->database->order_by( $order_by );
+                }
+            } else {
+                foreach ( $this->order_by as  $key => $value ) {
+                    $this->database->order_by( $key, $value );
+                }
+            }
+                
+
+        }
+        if( $this->limit ){
+            // $this->database->limit( $this->limit, $this->offset );
+            if (isset($this->offset)) {
+                $this->database->limit( $this->limit, $this->offset );
+            } else {
+                $this->database->limit( $this->limit );
+            }
+        }
+        $result = $this->database->get();
+        /*Here check for num rows returned by query if num rows <=1 then row() is returned otherwise result is returned*/
+        if( $result->num_rows <= 1 )
+        {
+            return $result->row();
+        } else {
+            if ($this->select_as_array) {
+                return $result->result_array();
+            } else {
+                return $result->result();
+            }
+        }
+    }
+    public function insert( $data = '' ){
+        if( $this->insert_batch )
+            return $this->database->insert_batch( $this->table , $data); 
+        else
+            return $this->database->insert( $this->table , $data );
+    }
+
+    public function insert_and_id( $data = '' ){
+        $status =  $this->database->insert( $this->table , $data );
+            if( $status )
+                $last_id = $this->database->insert_id();
+        return $last_id;
+    } 
+
+    public function delete(){
+        return $this->database->delete( $this->table , $this->condition );
+    }
+    public function update( $data ){
+        if( $this->update_batch ){
+             $this->database->trans_start();
+             $this->database->update_batch($this->table , $data, $this->condition ); 
+             $this->database->trans_complete();    
+             return $this->database->trans_status() ;
+        }else{
+
+            $this->database->where( $this->condition );
+            return $this->database->update( $this->table, $data );
+        }
+    }
+}
+?>
---
+++ application/modules/api/controllers/api.php
@@ -0,0 +1,49 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Api extends MY_Controller {
+
+	public function __construct()
+	{
+		 parent::__construct();		 
+		 $this->load->model('standard_model');
+	}
+
+
+	public function index_get()
+	{
+		$arr = array("message" => "hello ravi");
+		$this->response($arr);
+	}
+	public function data_get($p=null) {
+		if ($p==null) {
+
+			$arr = array("message" => "hello ravi");	
+		} else {
+			$arr = array("message" => $p);
+		}
+		$this->response($arr);
+	}
+	function monthWiseStoryIds_get() {
+
+		$monthdata['table'] = 'tbl_story' ;
+		$monthdata['field'] = "MONTH( date_of_creation ) as month";
+		$monthdata['group_by'] = array("month");
+
+		$this->standard_model->set_query_data($monthdata);
+		$vv = $this->standard_model->select();
+
+		
+		$ovralldata['table'] = 'tbl_story' ;
+		$ovralldata['field'] = "MONTH( date_of_creation ) as month, story_id";
+		$ovralldata['condition'] = array(	    	  
+	    	  "story_status != " => "deleted",
+	    	  "YEAR(date_of_creation)" => date("Y")
+	    );
+	    $ovralldata['where_in'] = array("month" => $vv);
+
+		$this->standard_model->set_query_data($ovralldata);
+		$data = $this->standard_model->select();
+
+		$this->response($data);
+	}
+}
